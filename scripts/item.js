939962c867f51234504e613b5cb5c40ab05d93e5
Egor <korobovegor44@gmail.com>
2024-05-07 22:06:40 +0300

    itemCard

--- scripts/item.js
+++ scripts/item.js
@@ -1,28 +1,35 @@
 const urlParams = new URLSearchParams(window.location.search);
 const selectedItem = JSON.parse(urlParams.get('item'));
 
-const infoBlock = document.querySelector('.iteminfo');
+const infoBlock = document.querySelector('.itemInfo');
 infoBlock.innerHTML = '';
 
-const img = document.createElement('img');
-          img.src = 'img/'+selectedItem.image;
-          img.alt = selectedItem.name;
-          infoBlock.appendChild(img);
+const imgBlock=document.createElement('div');
+imgBlock.className='imgBlock';
 
-const name = selectedItem.name;
+const img = document.createElement('img');
+          img.src = 'img/'+ selectedItem.image;
+          img.alt = selectedItem.name;
+          imgBlock.appendChild(img);
+
+          infoBlock.appendChild(imgBlock);
+
+const cont=document.createElement('div');
+        cont.className='info';
+
+const itemName = selectedItem.name;
 const h3 = document.createElement('h3');
-        h3.textContent = name;
-        infoBlock.appendChild(h3);
+        h3.textContent = itemName;
+        cont.appendChild(h3);
 
 const price = selectedItem.price;
 const p = document.createElement('p');
-          p.textContent = price;
-          infoBlock.appendChild(p);
+          p.textContent = price + ' BYN';
+          cont.appendChild(p);
 
 const description = selectedItem.description;
 const p2 = document.createElement('p');
           p2.textContent = description;
-          infoBlock.appendChild(p2);
+          cont.appendChild(p2);
 
-
-
+          infoBlock.appendChild(cont);
